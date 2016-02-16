995eb2748d912e5df8f67f8df0dfaed782754a1a
Evan Bender <evan.bender@percolate.com>
2016-02-16 10:47:13 -0500

    producer and consumer

--- pykafka-consumer.py
+++ pykafka-consumer.py
@@ -1,21 +1,22 @@
 import pykafka
 import logging
 import time
-import datetime
 
 logging.basicConfig(level=logging.DEBUG)
 
 client = pykafka.KafkaClient(hosts="kafka:9092",
                              zookeeper_hosts="zookeeper:2181")
 topic = client.topics['test.topic']
-producer = topic.get_producer()
-
+consumer = topic.get_balanced_consumer("test.topic")
 
 while True:
     try:
-        now = datetime.datetime.utcnow().isoformat()
-        producer.produce(now)
-        time.sleep(1)
-        logging.info("published at %s" % now)
+        got = consumer.consume(block=False)
+        if got is None:
+            logging.debug("waiting")
+            time.sleep(0.3)
+            continue
+
+        logging.info(got.value)
     except Exception:
         logging.exception("bummer")
